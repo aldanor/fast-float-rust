9acee6625433dbcddd0087494329896085786678
Ivan Smirnov <i.s.smirnov@gmail.com>
2021-01-10 16:43:03 +0000

    Add an optional exhaustive f32 roundtrip test

---
+++ tests/test_exhaustive.rs
@@ -0,0 +1,11 @@
+#[test]
+#[ignore]
+fn test_f32_exhaustive() {
+    let mut buf = ryu::Buffer::new();
+    for i in 0..0xFFFF_FFFF_u32 {
+        let a: f32 = unsafe { core::mem::transmute(i) };
+        let s = buf.format(a);
+        let b: f32 = fast_float::parse(s).unwrap();
+        assert!(a == b || (a.is_nan() && b.is_nan()));
+    }
+}
