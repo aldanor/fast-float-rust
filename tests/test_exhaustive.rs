ce9e1dd244da92e1d093df88fdd549c9060efaef
Ivan Smirnov <i.s.smirnov@gmail.com>
2021-01-12 00:05:31 +0000

    Remove f32 exhaustive lexical r/t (same as ryu)

--- tests/test_exhaustive.rs
+++ tests/test_exhaustive.rs
@@ -9,15 +9,3 @@ fn test_f32_exhaustive_ryu() {
         assert!(a == b || (a.is_nan() && b.is_nan()));
     }
 }
-
-#[test]
-#[ignore]
-fn test_f32_exhaustive_lexical() {
-    let mut buf = [0; 1024];
-    for i in 0..0xFFFF_FFFF_u32 {
-        let a: f32 = unsafe { core::mem::transmute(i) };
-        let s = lexical_core::write(a, &mut buf);
-        let b: f32 = fast_float::parse(s).unwrap();
-        assert!(a == b || (a.is_nan() && b.is_nan()));
-    }
-}
