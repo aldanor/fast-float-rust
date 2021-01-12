967d6853e02c66751228f841870e881748a5406a
Ivan Smirnov <i.s.smirnov@gmail.com>
2021-01-12 00:02:23 +0000

    Extend f64 fuzzer to use stdlib formatter as well

--- fuzz/fuzz_targets/roundtrip_f64.rs
+++ fuzz/fuzz_targets/roundtrip_f64.rs
@@ -4,16 +4,19 @@ use libfuzzer_sys::fuzz_target;
 // We only test the roundtrip of f64 with a fuzzer because f32 search space
 // is small enough that we can test it exhaustively
 
-fuzz_target!(|float: f64| {
-    // we use ryu instead of stdlib formatter because it exercises more paths:
-    // it will format long floats in scientific notation, if appropriate,
-    // while the std formatter will never do so.
-    let mut buf = ryu::Buffer::new();
-    let stringified_float = buf.format(float);
-    let roundtripped_float = ::fast_float::parse::<f64, _>(stringified_float).unwrap();
+fn check_roundtrip(float: f64, string: impl AsRef<str>) {
+    let result = ::fast_float::parse::<f64, _>(string.as_ref()).unwrap();
     if float.is_nan() {
-        assert!(roundtripped_float.is_nan());
+        assert!(result.is_nan());
     } else {
-        assert_eq!(float, roundtripped_float)
+        assert_eq!(float, result);
     }
+}
+
+fuzz_target!(|float: f64| {
+    // we use both ryu and stdlib since ryu prefers scientific notation while stdlib
+    // never uses it at all; hence more code paths will be executed
+    let mut buf = ryu::Buffer::new();
+    check_roundtrip(float, buf.format(float));
+    check_roundtrip(float, float.to_string());
 });
